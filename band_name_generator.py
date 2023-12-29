cc9f7fa8d20b0b6cccf2ddb588f0f13aee115014
Gabby <gulcindenirkiran@gmail.com>
2023-12-29 22:51:40 +0100

    Band Name Generator

---
+++ band_name_generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator.")
+
+city = input("What is name of the city you grew up in?")
+pet = input("What is your cat's name?")
+
+print(f"Your band name could be {city} {pet}" )
