7638cc5e92342aa78523ba9924d87e7f00c50648
Gabby <gulcindenirkiran@gmail.com>
2024-04-10 00:47:15 +0200

    Naive/Brute Force

--- bubble_sort.py
+++ bubble_sort.py
@@ -12,9 +12,6 @@ def bubbleSort(arr):
         # Last i elements are already in place
         for j in range(0, n-i-1):
  
-            # Traverse the array from 0 to n-i-1
-            # Swap if the element found is greater
-            # than the next element
             if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
